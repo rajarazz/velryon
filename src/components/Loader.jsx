74284bd24d77bf041ee65b4ab29d69a41c503726
Raja Raj <rajaraj725592@gmail.com>
2026-02-18 17:50:24 +0530

    preload all to preload

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,31 +1,48 @@
-import { Html, useProgress } from "@react-three/drei";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const CanvasLoader = () => {
-  const { progress } = useProgress();
+function EarthModel() {
+  const { scene } = useGLTF("/planet/scene.gltf");
+
   return (
-    <Html
-      as='div'
-      center
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+    <primitive
+      object={scene}
+      scale={2.5}
+      position={[0, 0, 0]}
+      rotation={[0, 0, 0]}
+    />
+  );
+}
+
+useGLTF.preload("/planet/scene.gltf");
+
+export default function EarthCanvas() {
+  return (
+    <Canvas
+      frameloop="always"
+      dpr={[1, 2]}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
       }}
     >
-      <span className='canvas-loader'></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
-        {progress.toFixed(2)}%
-      </p>
-    </Html>
-  );
-};
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 5]} intensity={1} />
 
-export default CanvasLoader;
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <EarthModel />
+        <Preload />
+      </Suspense>
+    </Canvas>
+  );
+}
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,8 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
 
-const Earth = () => {
+function EarthModel() {
   const { scene } = useGLTF("/planet/scene.gltf");
 
   return (
@@ -14,14 +13,14 @@ const Earth = () => {
       rotation={[0, 0, 0]}
     />
   );
-};
+}
 
-const EarthCanvas = () => {
+export default function EarthCanvas() {
   return (
     <Canvas
       shadows
+      frameloop="always"
       dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
       camera={{
         fov: 45,
         near: 0.1,
@@ -29,18 +28,19 @@ const EarthCanvas = () => {
         position: [-4, 3, 6],
       }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 5]} intensity={1} />
+
+      <Suspense fallback={null}>
         <OrbitControls
           autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Earth />
+        <EarthModel />
         <Preload all />
       </Suspense>
     </Canvas>
   );
-};
-
-export default EarthCanvas;
+}
